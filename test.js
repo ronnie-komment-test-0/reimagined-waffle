6c01646eafeef7025962f62f10e6090f9cbc9023
ronnie-komment-test-0 <153918503+ronnie-komment-test-0@users.noreply.github.com>
2024-02-08 17:48:27 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 
-const fn = () => { console.log(0); }
 
+function BinaryCountSetBits(a) {
+  'use strict'
+
+  if (!Number.isInteger(a)) throw new TypeError('Argument not an Integer')
+
+  let count = 0
+  while (a) {
+    a &= a - 1
+    count++
+  }
+
+  return count
+}
+
+export { BinaryCountSetBits }
