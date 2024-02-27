bae14aacb5c5633bfcfe8c2f0d821fc487090fa1
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-02-27 15:27:37 +0000

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,56 @@
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function findMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function isPalindrome(str) {
+    return str === reverseString(str);
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function calculateCircleArea(radius) {
+    return Math.PI * radius ** 2;
+}
+
+function countOccurrences(arr) {
+    return arr.reduce((acc, val) => {
+        acc[val] = (acc[val] || 0) + 1;
+        return acc;
+    }, {});
+}
+
+function capitalizeWords(sentence) {
+    return sentence.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
