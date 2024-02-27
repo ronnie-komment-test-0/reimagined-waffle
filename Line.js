c7d5846a69bf017e60d0f63b7846a7f17347b915
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-02-27 10:52:18 +0000

    Update Line.js

--- Line.js
+++ Line.js
@@ -4,6 +4,7 @@ import { getMonthYear } from "../../utils/Helpers";
 import { FaInfinity } from "react-icons/fa6";
 import classNames from "classnames";
 
+
 const Line = ({ data, metric, label }) => {
   const [amount, setAmount] = useState(0);
   const [delta, setDelta] = useState(0.0);
